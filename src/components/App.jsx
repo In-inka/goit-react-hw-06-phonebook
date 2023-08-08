f6d3282bdf44ff5f206683fafaab1f3e5f9f74a3
Inna Shkilna <shkinai.1995@gmail.com>
2023-08-08 09:48:20 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,9 @@
-//import { useState } from 'react';
-//import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
-//import data from 'data.json';
 import { Title, PhoneBook } from './App.styled';
 
-//const LS_KEY = 'contact save';
-
 export const App = () => {
-  /*   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem(LS_KEY)) ?? data;
-  }); */
-  //const [filterContact, setFilterContact] = useState('');
-
-  /*  useEffect(() => {
-//    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
- // }, [contacts]);
-
-  const submitContact = el => {
-    if (contacts.some(({ name }) => name === el.name)) {
-      Notify.failure(`${el.name} is already in contacts.`);
-      return;
-    }
-
- //   setContacts([...contacts, { ...el }]);
-  }; 
-*/
-
   return (
     <PhoneBook>
       <Title>Phonebook</Title>
